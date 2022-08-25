353acb452711fb58b5e0affecaac0832277e8441
Maxim Manylov <manylov@gmail.com>
2022-08-25 21:33:13 +0700

    fix meta urls and add address for fuji

--- src/blockchain/contracts-addresses.ts
+++ src/blockchain/contracts-addresses.ts
@@ -8,19 +8,19 @@ const NETWORK_DATA: Record<
 > = {
   harmony: {
     GEARS: "",
-    meta: "https://meta.marscolony.io/",
+    meta: "https://gears-harmony.marscolony.io/",
   },
   polygon: {
     GEARS: "",
-    meta: "https://meta-polygon.marscolony.io/",
+    meta: "https://gears-polygon.marscolony.io/",
   },
   mumbai: {
     GEARS: "",
-    meta: "https://meta-mumbai.marscolony.io/",
+    meta: "https://gears-mumbai.marscolony.io/",
   },
   fuji: {
-    GEARS: "",
-    meta: "https://meta-fuji.marscolony.io/",
+    GEARS: "0x54d4bf6035D6fD5C1f06A01A382C2Fe91CA6dA80",
+    meta: "https://gears-fuji.marscolony.io/",
   },
   development: {
     GEARS: "0xA402d858154A99d86aADF5dFD749B33Be5d7d3a4",
